cd3742642774cfb8927eb644d4bc27c325257d32
fadi <fadi@service.com>
2015-12-18 00:37:30 -0500

    finalized models

--- mma/mma/settings.py
+++ mma/mma/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mmaapp',
 ]
 
 MIDDLEWARE_CLASSES = [
--- mma/mmaapp/models.py
+++ mma/mmaapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Promoter(models.Model):
+	user = models.OneToOneField(User, null=True)
+	promote = models.BooleanField(default=True)
+
+class Event(models.Model):
+	promoter = models.OneToOneField(Promoter)
+	name = models.TextField()
+	poster = models.TextField()
+
+class Fighters(models.Model):
+	picture = models.TextField(null=True, blank=True)
+	name = models.TextField()
+	weight = models.TextField()
+	height = models.CharField(max_length=2)
+	reach = models.CharField(max_length=2)
+	age = models.CharField(max_length=2)
+
+class Fights(models.Model):
+	Event = models.OneToOneField(Event)
+	Fighter1 = models.OneToOneField(Fighters)
+	Fighter2 = models.OneToOneField(Fighters, related_name="opponent")
