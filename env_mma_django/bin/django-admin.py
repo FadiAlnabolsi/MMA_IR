1bcdf509ca33f8180d3e2ca4b8694040262601a9
fadi <fadi@service.com>
2015-12-17 23:38:38 -0500

    started building website

---
+++ env_mma_django/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fadi/Documents/School/ir/env_mma_django/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ mma/mmaapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MmaappConfig(AppConfig):
+    name = 'mmaapp'
